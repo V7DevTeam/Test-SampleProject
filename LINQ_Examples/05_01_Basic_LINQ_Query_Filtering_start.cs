b3a38b52ab25e999c02d847ae18dd40e15efb727
charles.heon <charles.heon@v7international.com>
2018-09-13 15:59:39 -0400

    Added int mutliplication

--- LINQ_Examples/05_01_Basic_LINQ_Query_Filtering_start.cs
+++ LINQ_Examples/05_01_Basic_LINQ_Query_Filtering_start.cs
@@ -79,6 +79,11 @@ namespace LINQ_Examples
                 where c.State == "OR" && c.Price > 1000
                 select c;
 
+            int i = 10;
+            i  = i * 1000000;
+
+            Console.WriteLine(i);
+
             foreach (Customer c in stateQuery)
             {
                 Console.WriteLine("{0}, {1}, {2:c}", c.First, c.Last, c.Price);
